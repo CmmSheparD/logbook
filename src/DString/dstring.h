327ab0e2eef115518be14178c53d5aff812910a9
CmmSheparD <ustynow.gleb@gmail.com>
2021-05-09 12:35:40 +0300

    feat: Changed free space coounting policy.

--- src/DString/dstring.h
+++ src/DString/dstring.h
@@ -10,8 +10,16 @@
 
 #define DSTRING_INIT_SIZE 128
 
+/*
+ * Structure representing a vector of wide characters - dynamic string.
+ * Data members:
+ * `raw_string` - pointer to an array allocated in the heap.
+ * `len`        - length of the string stored in the vector. Has the same
+ *                value as a `wcslen(raw_string)` would return.
+ * `max`        - the size of an array.
+ */
 typedef struct DString {
-	size_t size;
+	size_t len;
 	size_t max;
 	wchar_t *raw_string;
 } dstring_t;
@@ -20,6 +28,7 @@ dstring_t *ds_createString();
 void ds_freeString(dstring_t *ds);
 
 bool ds_isEmpty(const dstring_t *ds);
+size_t ds_spaceAvalable(const dstring_t *ds);
 
 int ds_appendChar(dstring_t *ds, wchar_t c);
 int ds_appendString(dstring_t *ds, wchar_t *string);
